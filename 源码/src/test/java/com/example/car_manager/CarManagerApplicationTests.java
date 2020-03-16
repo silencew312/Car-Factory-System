0db2c2ab141c3eea480dc5f83d48f5dbc7243d48
silencew312 <56785491+silencew312@users.noreply.github.com>
2020-03-16 16:26:03 +0800

    Create CarManagerApplicationTests.java

---
+++ 源码/src/test/java/com/example/car_manager/CarManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
